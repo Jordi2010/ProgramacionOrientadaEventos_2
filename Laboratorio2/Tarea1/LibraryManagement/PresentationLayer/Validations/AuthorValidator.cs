855675c39a822af2f3a5f4e2f253901e99becb51
chvzzsv <roscamchav70@gmail.com>
2023-10-10 19:34:56 -0600

    validations-soon

--- Laboratorio2/Tarea1/LibraryManagement/PresentationLayer/Validations/AuthorValidator.cs
+++ Laboratorio2/Tarea1/LibraryManagement/PresentationLayer/Validations/AuthorValidator.cs
@@ -8,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace PresentationLayer.Validations
 {
-    public class AuthorValidator
+    public class AuthorValidator : AbstractValidator<Author>
     {
+        public AuthorValidator() 
+        {
+            RuleFor(author => author.IdAuthor)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("El ID no puede estar vacío")
+                .GreaterThan(0).WithMessage("El ID debe ser mayor que cero");
+
+            RuleFor(author => author.FirstName).NotEmpty()
+                .WithMessage("El nombre no puede estar vacío");
+
+            RuleFor(author => author.LastName).NotEmpty()
+                .WithMessage("El apellido no puede estar vacío");
+        }
     }
 }
--- Laboratorio2/Tarea1/LibraryManagement/PresentationLayer/Validations/LoanValidator.cs
+++ Laboratorio2/Tarea1/LibraryManagement/PresentationLayer/Validations/LoanValidator.cs
@@ -8,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace PresentationLayer.Validations
 {
-    public class LoanValidator
+    public class LoanValidator : AbstractValidator<Loan>
     {
+        public LoanValidator() 
+        {
+            RuleFor(loan => loan.IdLoan)
+               .Cascade(CascadeMode.Stop)
+               .NotEmpty().WithMessage("El ID no puede estar vacío")
+               .GreaterThan(0).WithMessage("El ID debe ser mayor que cero");
+
+            RuleFor(loan => loan.IdBook)
+               .NotEmpty().WithMessage("El ID no puede estar vacío")
+               .GreaterThan(0).WithMessage("El ID debe ser mayor que cero");
+
+            RuleFor(loan => loan.Customer)
+               .NotEmpty().WithMessage("Este campo no puede estar vacío")
+               .MinimumLength(5).WithMessage("Por favor introduzca mímino 5 carácteres");
+
+            RuleFor(loan => loan.LoanDate)
+               .NotEmpty().WithMessage("Ingrese una fecha de préstamo válida");
+        }
     }
 }
